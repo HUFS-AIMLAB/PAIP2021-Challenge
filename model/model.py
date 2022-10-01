6bf51d29213b79e31f5d50ad2db55e69c9bc99bf
hwanseung <wazs555@hufs.ac.kr>
2022-10-01 13:50:11 +0900

    unet learner #4(7) debug

--- model/model.py
+++ model/model.py
@@ -6,6 +6,7 @@ import segmentation_models_pytorch as smp
 
 class EfficientNetB0(nn.Module):
     def __init__(self, pre_trained, num_classes):
+        super(EfficientNetB0, self).__init__()
         self.model = timm.create_model("tf_efficientnet_b0_ns", pretrained = pre_trained, num_classes = num_classes)
     
     
@@ -15,6 +16,7 @@ class EfficientNetB0(nn.Module):
 
 class UNetEfficientNet(nn.Module):
     def __init__(self, num_classes, encoder_path):
+        super(UNetEfficientNet, self).__init__()
         self.model = smp.Unet(encoder_name = "timm-efficientnet-b0", encoder_weights = "noisy-student",in_channels = 3, classes = num_classes)
         self.model.encoder.load_state_dict(torch.load(encoder_path))
 
