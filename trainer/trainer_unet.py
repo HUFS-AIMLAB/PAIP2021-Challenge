d50bd5752d2d64e512e3367a7d5cf67d85a764c5
hwanseung <wazs555@hufs.ac.kr>
2022-10-01 16:52:37 +0900

    unet learner #4(28) debug

--- trainer/trainer_unet.py
+++ trainer/trainer_unet.py
@@ -81,18 +81,17 @@ class UNetTrainer():
                 continue
             if self.args.train_type != 'all' and patient.split('_')[0] != self.args.train_type.title():
                 continue
-            for label in sorted(os.listdir(os.path.join(root_dir, patient, 'sw', f"level_{level_dim}"))):
-                for image in sorted(os.listdir(os.path.join(root_dir, patient, 'sw', f"level_{level_dim}", label, "img_sw"))):
-                    if image.split('.')[-1] != 'png':
+            for image in sorted(os.listdir(os.path.join(root_dir, patient, 'sw', f"level_{level_dim}", 'pni', "img_sw"))):
+                if image.split('.')[-1] != 'png':
+                    continue
+                else:
+                    if self.args.random_sampling and random.randint(0, 9) < 9:
                         continue
-                    else:
-                        if self.args.random_sampling and random.randint(0, 9) < 9:
-                            continue
-                        case = {
-                            'image' : os.path.join(root_dir, patient, 'sw', f"level_{level_dim}", label, "img_sw", image),
-                            'label' : os.path.join(root_dir, patient, 'sw', f"level_{level_dim}", label, "mask_sw", image)
-                        }
-                        data_list.append(case)
+                    case = {
+                        'image' : os.path.join(root_dir, patient, 'sw', f"level_{level_dim}", 'pni', "img_sw", image),
+                        'label' : os.path.join(root_dir, patient, 'sw', f"level_{level_dim}", 'pni', "mask_sw", image)
+                    }
+                    data_list.append(case)
                 
         return data_list
 
